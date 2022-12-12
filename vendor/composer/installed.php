4ef8e1dba65c2623e4c5d7c247858594a184d986
Rtiemm <emshanovilya2004@gmail.com>
2022-12-12 22:31:18 +0300

    2

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'vendor_name/git-learning',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'f29f8a2ddfac53dfba35b6ebd2ada7f2179edc57',
+        'reference' => '028c691809ca6598e5ccc8272e73df2a0380740e',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'vendor_name/git-learning' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'f29f8a2ddfac53dfba35b6ebd2ada7f2179edc57',
+            'reference' => '028c691809ca6598e5ccc8272e73df2a0380740e',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
